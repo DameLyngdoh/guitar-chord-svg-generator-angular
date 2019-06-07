a0dfcba8333158ca9f8ba28214f338bae8b39715
Dame Lyngdoh <lyngdoh.dame@gmail.com>
2019-06-07 18:27:09 +0530

    Initial commit

---
+++ projects/guitar-chord-generator/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of guitar-chord-generator
+ */
+
+export * from './lib/guitar-chord-generator.service';
+export * from './lib/guitar-chord-generator.component';
+export * from './lib/guitar-chord-generator.module';
